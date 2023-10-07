aeb75f29e6c6580a6e0eb4effdf01156df2f19ce
Svene <aidrecabrera@gmail.com>
2023-10-07 22:39:28 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace Dapper_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=127.0.0.1;Port=3306;Database=dapper;Uid=aidrecabrera;Pwd=aidrecabrera;";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            var employee = new Employee
+            {
+                Name = "Aidre",
+                Age = 30,
+                Salary = 130000
+            };
+
+            string insertQuery = "INSERT INTO employees (Name, Age, Salary) VALUES (@Name, @Age, @Salary)";
+
+            connection.Execute(insertQuery, employee);
+
+            string selectQuery = "SELECT * FROM employees";
+
+            List<Employee> employees = connection.Query<Employee>(selectQuery).ToList();
+            
+       
+        }
+    }
+}
